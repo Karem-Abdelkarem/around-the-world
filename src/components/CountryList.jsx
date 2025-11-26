84e2d10f9d2522f45f26548e6d7854b269f3e3e2
Kareem Abdelkarem <km6263458@gmail.com>
2025-11-27 00:38:44 +0200

    feat: Add EmptySearch statment

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,18 +1,23 @@
 import CountryCard from "./CountryCard";
+import EmptySearch from "./EmptySearch";
 
 const CountryList = ({ data }) => {
   return (
     <section className="mt-8 grid grid-cols-1 justify-between gap-y-12 sm:grid-cols-2 sm:gap-x-16 md:mt-12 md:grid-cols-[repeat(2,minmax(0,auto))] lg:grid-cols-[repeat(3,minmax(0,auto))] xl:grid-cols-[repeat(4,minmax(0,auto))] xl:gap-[70px]">
-      {data.map((country) => (
-        <CountryCard
-          key={country.name.official}
-          name={country.name.common}
-          population={country.population}
-          region={country.region}
-          capital={country.capital}
-          flag={country.flags.svg}
-        />
-      ))}
+      {data && data.length ? (
+        data.map((country) => (
+          <CountryCard
+            key={country.name.official}
+            name={country.name.common}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            flag={country.flags.svg}
+          />
+        ))
+      ) : (
+        <EmptySearch />
+      )}
     </section>
   );
 };
